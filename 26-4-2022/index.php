d5408997565b1b5d7ef510be5cf843e35a545d73
unknown <mohammedamin.ababneh@gmail.com>
2022-04-26 15:44:16 +0300

    main

---
+++ 26-4-2022/index.php
@@ -0,0 +1,10 @@
+<?php 
+$colors = array('white', 'green', 'red');
+
+echo "<ul>
+<li>$colors[0]</li>
+<li>$colors[1]</li>
+<li>$colors[2]</li>
+</ul>"
+
+?>
---
+++ ExtraTasks/26-4-2022/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+$data = array("First" => array(1,2,3) 
+, "second" => "ahmad" ,
+"Third" => "tamer");
+
+
+$data["abdallah"] = 0;
+
+echo "<pre>";
+print_r($data) ;
+echo "</pre>";
+
+
+
+
+?> 
